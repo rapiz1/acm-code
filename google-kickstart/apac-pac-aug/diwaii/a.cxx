55d74c33353d4092f053c4af09e4603f2c4b7edb
Yujia Qiao <rapiz@foxmail.com>
2021-08-21 23:12:43 +0800

    update

--- google-kickstart/apac-pac-aug/diwaii/a.cxx
+++ google-kickstart/apac-pac-aug/diwaii/a.cxx
@@ -3,19 +3,31 @@
 #include <string>
 using namespace std;
 string s;
-typedef long long l;
-ll left, right;
+typedef long long ll;
+ll l, r;
 void input() {
   cin >> s;
-  cin >> left;
-  >> right;
+  cin >> l >> r;
 }
-int solve() { return 0; }
+static const char TARGET = 'B';
+ll solve() {
+  int bc = 0;
+  for (auto c : s) bc += c == TARGET;
+  l--, r--;
+  ll l_idx = l / s.size(), r_idx = r / s.size();
+  ll complete_count = r_idx - l_idx - 1;
+  l %= s.size();
+  r %= s.size();
+  ll answer = complete_count * bc;
+  for (int i = l; i < s.size(); i++) answer += s[i] == TARGET;
+  for (int i = 0; i <= r; i++) answer += s[i] == TARGET;
+  return answer;
+}
 int main() {
   int k;
   cin >> k;
   for (int i = 1; i <= k; i++) {
     input();
-    printf("Case #%d: %d", i, solve());
+    printf("Case #%d: %lld\n", i, solve());
   }
 }
