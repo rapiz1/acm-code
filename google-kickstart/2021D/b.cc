5b4f357858d962a60004ef75f71c328cf4b0e9a3
Yujia Qiao <rapiz3142@gmail.com>
2021-09-19 09:08:06 +0800

    update

--- google-kickstart/2021D/b.cc
+++ google-kickstart/2021D/b.cc
@@ -2,36 +2,51 @@
 using namespace std;
 typedef long long ll;
 const int MAXN = 1e5 + 10;
-ll l[MAXN], r[MAXN], c, diff[MAXN * 2];
+ll l[MAXN], r[MAXN], c;
 ll solve() {
   int n;
   cin >> n >> c;
-  vector<ll> allpos;
+  vector<ll> allpos, diff;
   for (int i = 0; i < n; i++) {
     cin >> l[i] >> r[i];
     allpos.push_back(l[i]);
+    allpos.push_back(l[i] + 1);
     allpos.push_back(r[i]);
   }
   sort(allpos.begin(), allpos.end());
   allpos.resize(unique(allpos.begin(), allpos.end()) - allpos.begin());
+  diff.resize(allpos.size());
   unordered_map<ll, int> pos2id;
   for (int i = 0; i < allpos.size(); i++) pos2id[allpos[i]] = i, diff[i] = 0;
   for (int i = 0; i < n; i++) {
     int lid = pos2id[l[i]], rid = pos2id[r[i]];
     diff[lid + 1]++;
     diff[rid]--;
+    // printf("add %d %d\n", lid, rid - 1);
   }
-  vector<ll> counts;
+
+  typedef pair<ll, ll> pa;
+  vector<pa> counts;
+
   counts.reserve(allpos.size());
   for (int i = 1; i < allpos.size(); i++) {
+    ll left_end = allpos[i - 1];
+    ll len = allpos[i] - left_end;
+    counts.push_back(pa{diff[i - 1], len});
+    // printf("diff[%d]: %d, len: %d\n", i - 1, diff[i - 1], len);
+
     diff[i] += diff[i - 1];
-    counts.push_back(diff[i]);
   }
-  counts.push_back(diff[0]);
-  sort(counts.begin(), counts.end(), less<ll>{});
+  // counts.push_back(diff[0]);
+  sort(counts.begin(), counts.end(), greater<pa>{});
   ll answer = 0;
-  for (int i = 0; i < c; i++) answer += counts[i];
-  return answer;
+  for (int i = 0; i < counts.size(); i++) {
+    if (!c) break;
+    ll this_num = min(c, counts[i].second);
+    c -= this_num;
+    answer += counts[i].first * this_num;
+  }
+  return answer + n;
 }
 
 int main() {
